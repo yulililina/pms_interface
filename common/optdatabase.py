0d6ff6dde89fb694b613d204995f647a1eb0e00f
wanglei03 <wanglei03@lightinthebox.com>
2021-10-26 19:55:41 +0800

    增加requestkey文件，主要封装request请求

--- common/optdatabase.py
+++ common/optdatabase.py
@@ -45,14 +45,99 @@ class OperationDbInterface(object):
             logger = deflogger.logger_test()
             logger.exception(error)
 
+    # 定义单条数据操作，包含删除、更新操作
+    def op_sql(self, condition):
+        """
+        :param condition: sql语句,该通用方法可用来替代updateone，deleteone
+        :return:字典形式
+        """
+        try:
+            self.cur.execute(condition)  # 执行sql语句
+            self.conn.commit()  # 提交游标数据
+            result = {'code': '0000', 'message': '执行通用操作成功', 'data': []}
+        except pymysql.Error as error:
+            self.conn.rollback()  # 执行回滚操作
+            result = {'code': '9999', 'message': '执行通用操作异常', 'data': []}
+            print(u"数据库错误|op_sql %d: %s" % (error.args[0], error.args[1]))
+            logger = deflogger.logger_test()
+            logger.exception(error)
+        return result
 
+    # 查询表中单条数据
+    def select_one(self, condition):
+        """
+        :param condition: sql语句
+        :return: 字典形式的单条查询结果
+        """
+        try:
+            rows_affect = self.cur.execute(condition)
+            if rows_affect > 0:  # 查询结果返回数据数大于0
+                results = self.cur.fetchone()  # 获取一条结果
+                result = {'code': '0000', 'message': '执行单条查询操作成功', 'data': results}
+            else:
+                result = {'code': '0000', 'message': '执行单条查询操作成功', 'data': []}
+        except pymysql.Error as error:
+            self.conn.rollback()  # 执行回滚操作
+            result = {'code': '9999', 'message': '执行单条查询异常', 'data': []}
+            print(u"数据库错误|select_one %d: %s" % (error.args[0], error.args[1]))
+            logger = deflogger.logger_test()
+            logger.exception(error)
+        return result
 
+    # 查询表中多条数据
+    def select_all(self, condition):
+        '''
+        :param condition: sql语句
+        :return:字典形式的批量查询结果
+        '''
+        try:
+            rows_affect = self.cur.execute(condition)
+            if rows_affect > 0:  # 查询结果返回数据数大于0
+                self.cur.scroll(0, mode='absolute')  # 光标回到初始位置
+                results = self.cur.fetchall()  # 返回游标中所有结果
+                result = {'code': '0000', 'message': '执行批量查询操作成功', 'data': results}
+            else:
+                result = {'code': '0000', 'message': '执行批量查询操作成功', 'data': []}
+        except pymysql.Error as error:
+            self.conn.rollback()  # 执行回滚操作
+            result = {'code': '9999', 'message': '执行批量查询异常', 'data': []}
+            print(u"数据库错误|select_all %d: %s" % (error.args[0], error.args[1]))
+            logger = deflogger.logger_test()
+            logger.exception(error)
+        return result
 
+    # 定义表中插入数据操作
+    def insert_data(self, condition, params):
+        '''
+        :param condition: insert语句
+        :param params: insert数据，列表形式[('3','Tom','1 year 1 class','6'),('3','Jack','2 year 1 class','7'),]
+       :return:字典形式的批量插入数据结果
+        '''
+        try:
+            results = self.cur.executemany(condition, params)  # 返回插入的数据条数
+            self.conn.commit()
+            result = {'code': '0000', 'message': '执行批量查询操作成功', 'data': results}
+        except pymysql.Error as error:
+            self.conn.rollback()  # 执行回滚操作
+            result = {'code': '9999', 'message': '执行批量插入异常', 'data': []}
+            print(u"数据库错误|insert_more %d: %s" % (error.args[0], error.args[1]))
+            logger = deflogger.logger_test()
+            logger.exception(error)
+        return result
 
+    # 数据库关闭
+    def __del__(self):
+        if self.cur != None:
+            self.cur.close()  # 关闭游标
+        if self.conn != None:
+            self.conn.close()  # 释放数据库资源
 
 
 if __name__ == "__main__":
     test = OperationDbInterface() # 初始化数据库连接
+    sql = 'SELECT* from v3_procurement_order where procurement_order_id in (16789008)'
+    result_select_one = test.select_one(sql)
+    print(result_select_one['data'])
 
 
 
