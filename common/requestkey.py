cd716826e29f478fc3d900f2a544ad7b6058e3b3
wanglei03 <wanglei03@lightinthebox.com>
2021-10-27 09:38:59 +0800

    增加requestkey文件，主要封装request请求1

--- common/requestkey.py
+++ common/requestkey.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # _*_ coding:utf-8 _*_
 # creater:yulili
+
+import requests
+from common import deflogger
+
+class RequestKey(object):
+
+    # post 请求，参数在body中
+    def __http__post(self):
+
+    # get 请求
+    def __http__get(self):
+
